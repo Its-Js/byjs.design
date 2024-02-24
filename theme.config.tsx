a672f2425445f1d7c918ad507b258cf134c1c46b
Js <89004466+Its-Js@users.noreply.github.com>
2024-02-24 16:07:45 +0800

    ~

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>ByJs</span>,
+  logo: <span>Js.</span>,
   project: {
     link: "https://github.com/Its-Js/byjs.design",
   },
